5f45815dc98f73718c5611cf301b7e69da5ca118
Kai <119144587+nguyenhuynh7014@users.noreply.github.com>
2024-03-22 19:19:56 +1100

    Add files via upload

---
+++ Item.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Swin_Adventure
+{
+    public class Item : Game_Object
+    {
+        public Item(string[] id,string name, string desc):base(id,name,desc)
+        {
+
+        }
+    }
+}
